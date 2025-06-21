1ebf68fd4d5539c989c359f3b055dab53f55f085
qasim-ali0 <45132095+qasim-ali0@users.noreply.github.com>
2025-06-21 20:53:38 +0000

    Deploying to gh-pages from @ qasim-ali0/qasim-ali0.github.io@0a0e9b5899da349bc8a6978c20fa456b1188a2d7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -420,51 +420,11 @@ ninja.data = [{
           section: "News",},{id: "news-i-will-be-at-rlc-2025-workshop-on-reward-free-reinforcement-learning-edmonton-ab-presenting-mr-crl",
           title: 'I will be at RLC 2025 Workshop on Reward-free Reinforcement Learning (Edmonton, AB),...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-parking-lot-hawk-drone",
+          title: 'Parking Lot Hawk Drone',
+          description: "Autonomous drone for real-time parking lot occupancy monitoring",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
