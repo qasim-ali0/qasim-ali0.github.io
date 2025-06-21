4962d8f2a603c46eb7b5580d3697da93f5f11ea7
qasim-ali0 <45132095+qasim-ali0@users.noreply.github.com>
2025-06-21 22:05:45 +0000

    Deploying to gh-pages from @ qasim-ali0/qasim-ali0.github.io@4986448011d9d7c630da4b419b81194e5f314e95 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,7 +37,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "books-the-godfather",
+        },{id: "post-",
+        
+          title: "",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/2025-02-28-llm_quiz/";
+          
+        },
+      },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
