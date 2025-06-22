e1968537b75ac614cf652ae9b7c1bc4928ffb53f
qasim-ali0 <45132095+qasim-ali0@users.noreply.github.com>
2025-06-22 05:16:17 +0000

    Deploying to gh-pages from @ qasim-ali0/qasim-ali0.github.io@27f4cefafbf9a4a6a9d4d08f30a4a78d5964f93f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -92,13 +92,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/muhammad-ali23", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
