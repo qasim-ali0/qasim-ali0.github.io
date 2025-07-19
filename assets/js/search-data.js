40b04d9b0279397adabceb2a690c574fc4d281aa
qasim-ali0 <45132095+qasim-ali0@users.noreply.github.com>
2025-07-19 23:38:38 +0000

    Deploying to gh-pages from @ qasim-ali0/qasim-ali0.github.io@35d28495916251bce344a99b5919a98f27045af1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -75,6 +75,11 @@ ninja.data = [{
           description: "An open-source world model specifically designed for humanoids.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_humanoid_world_model/";
+            },},{id: "projects-humanoid-world-model",
+          title: 'Humanoid World Model',
+          description: "An open-source world model specifically designed for humanoids.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/humanoid_world_model/";
             },},{
         id: 'social-email',
         title: 'email',
